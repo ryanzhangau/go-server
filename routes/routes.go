1f4c8eb4a98034da41749f67c877be865a7a7578
ryanzhangau <ryanzhangau@gmail.com>
2019-08-31 07:43:45 +1000

    change port

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,6 @@ import (
 func RouteHandler() {
 	http.HandleFunc("/", api.Homepage)
 	http.HandleFunc("/articles", api.AllArticles)
-	fmt.Println("Server is running on port 8080 ...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server is running on port 9090 ...")
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
