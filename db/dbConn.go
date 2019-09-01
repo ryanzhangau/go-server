2dc592e887f19531b3061c63efb39e211819b46e
ryanzhangau <ryanzhangau@gmail.com>
2019-09-01 19:37:19 +1000

    update database connect as singlton

--- db/dbConn.go
+++ db/dbConn.go
@@ -2,20 +2,20 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var client *mongo.Client
+
 //Connection - dabase connection
-func Connection() (client *mongo.Client) {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		fmt.Println("Database connection error")
-		return nil
+func Connection() (client *mongo.Client, err error) {
+	if client == nil {
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+		return client, err
 	}
-	return client
+	return client, nil
 }
