95f030da6bbaf83aa7805870f1b5459113bcd99f
ryanzhangau <ryanzhangau@gmail.com>
2019-09-01 19:05:19 +1000

    Create database connection

---
+++ db/dbConn.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//Connection - dabase connection
+func Connection() (client *mongo.Client) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println("Database connection error")
+		return nil
+	}
+	return client
+}
